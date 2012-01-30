174c3a8997ee8487d953e3890da38a8bc6f7ee65
Danny Yoo <dyoo@cs.wpi.edu>
2012-01-29 23:01:56 -0500

    adding notes on the while loop implementation I borrowed from Matthew Might

--- while-loop.rkt
+++ while-loop.rkt
@@ -9,32 +9,50 @@
 ;; http://matt.might.net/articles/implementing-exceptions/
 ;;
 ;; with a few adjustments so that break and continue are
-;; hygienic.
+;; hygienic, and the extent of the break and continue are
+;; around the body.
+;;
 (define-syntax-parameter break
   (lambda (stx) 
-    (raise-syntax-error #f "Used outside the context of a while" stx)))
+    (raise-syntax-error #f "Used outside the context of a while body" stx)))
 
 (define-syntax-parameter continue
   (lambda (stx) 
-    (raise-syntax-error #f "Used outside the context of a while" stx)))
+    (raise-syntax-error #f "Used outside the context of a while body" stx)))
 
 (define-syntax (while stx)
   (syntax-case stx ()
     [(_)
      (raise-syntax-error #f "missing test and body" stx)]
+
+    [(_ cond)
+     (raise-syntax-error #f "missing body" stx)]
+    
     [(_ cond body ...)
-     (begin
-       ;; When the body is empty, let's raise a syntactic error.
-       (when (null? (syntax->list #'(body ...)))
-         (raise-syntax-error #f "empty body" stx))
-     #'(call/ec (λ (fresh-break)
-                  (syntax-parameterize ([break (make-rename-transformer #'fresh-break)])
-                    (letrec ([loop (lambda ()
-                                     (when cond
-                                       (call/ec (lambda (fresh-continue)
-                                                  (syntax-parameterize ([continue
-                                                                         (make-rename-transformer #'fresh-continue)])
-                                                    (begin body ...))))
-                                       (loop)))])
-                      
-                      (loop))))))]))
+     (syntax/loc stx
+       (let/ec fresh-break 
+         (let loop ()
+           (when cond
+             (let/ec fresh-continue
+               (syntax-parameterize 
+                   ([break (make-rename-transformer #'fresh-break)]
+                    [continue (make-rename-transformer #'fresh-continue)])
+                 (begin body ...)))
+             (loop)))))]))
+
+
+;; By the way: it's a bit unclear what should happen if
+;; someone tries using break and continue within the
+;; context of a nested while loop's test... e.g.
+;; (while true
+;;    (while (begin (break))
+;;      'huh?)
+;;    'what?)
+;;
+;; This particular implementation is implemented such that
+;; the call to break will exit the outer loop.  It is an odd
+;; thing to think about, isn't it?  :)
+;;
+;; It should be easy to syntactically restrict if this really
+;; becomes an issue, by syntax-parameterizing error productions
+;; in the context of evaluating the _cond_ition.
